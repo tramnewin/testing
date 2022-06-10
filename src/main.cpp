02dc3951fe0ccb966e75462ba28884bdfd7efd47
Tram Nguyen <tram.nguyen7@terumobct.com>
2022-06-10 14:51:04 -0600

    put in some code

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,13 @@
 
 int main (){
     std::cout<< "hello world\n";
+    for(int i =0; i< 10 i++){
+        if (i%2 == 0){
+            std::cout<<i<< " is an even number\n";
+        }else{
+            std::cout<<i<<" is not an even number\n";
+        }
+    }
     return 0; 
+
 }
