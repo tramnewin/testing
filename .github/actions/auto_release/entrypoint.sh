dbb22cc6b1ee4a97aba780261f069b6e2631b89b
Tram Nguyen <tram.nguyen7@terumobct.com>
2022-06-13 12:47:43 -0600

    fix var

--- .github/actions/auto_release/entrypoint.sh
+++ .github/actions/auto_release/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -u
-repo_token = $1
+repo_token =$1
+
 if ["$GITHUB_EVENT_NAME" != "milestone"]; then
     echo "::debug::The event name was '$GITHUB_EVENT_NAME'"
     exit 0
@@ -10,7 +11,7 @@ if [ $event_type != "closed"]; then
     echo "::debug::The event type was '$event_type'"
     exit 0
 fi
-milestone_name = $(jq --raw-output .milestone)
+milestone_name = $(jq --raw-output .milestone.title $GITHUB_EVENT_PATH)
 IFS= '/' read owner repository <<< "$GITHUB_REPOSITORY"
 release_url= $(dotnet gitreleasemanager create \ --milestone $milestone_name \ --targercommitish $GITHUB_SHA \ --token $repo_token --owner $owner \ --repositoroy $repository)
 echo "::set-output name=release-url::$release_url"
