899a93db0ca474d394e24b541aecc68ec2e7bf5b
Tram Nguyen <tram.nguyen7@terumobct.com>
2022-07-20 11:42:55 -0600

    attempt code coverage via makefile and scripting file

--- unitTest/main.cpp
+++ unitTest/main.cpp
@@ -6,9 +6,9 @@ int main (){
     std::cout<< "hello world\n";
     for(int i =0; i< 10; i++){
         if (i%2 == 0){
-            std::cout<<i<< " is an even number\n";
+            std::cout<<i*2<< "\n";
         }else{
-            std::cout<<i<<" is not an even number\n";
+            std::cout<<i*3<<"\n";
         }
     }
     return 0; 
